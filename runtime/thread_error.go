bb4660f70ccf32c32f5fe87604dabb00d32fd6bd
Hiroshi Ioka <hirochachacha@gmail.com>
2016-11-29 16:11:14 +0900

    runtime: refactor error tracking

--- runtime/thread_error.go
+++ runtime/thread_error.go
@@ -5,25 +5,28 @@ import (
 	"github.com/hirochachacha/plua/position"
 )
 
+func (th *thread) trackError(err *object.RuntimeError) {
+	l := err.Level - 1
+	for {
+		d := th.getInfo(l, "Sl")
+		if d == nil {
+			break
+		}
+		err.Traceback = append(err.Traceback, position.Position{
+			SourceName: "@" + d.ShortSource,
+			Line:       d.CurrentLine,
+			Column:     -1,
+		})
+
+		l++
+	}
+}
+
 func (th *thread) error(err *object.RuntimeError) {
 	if th.status != object.THREAD_ERROR {
 		if err.Level > 0 {
-			l := err.Level - 1
-			for {
-				d := th.getInfo(l, "Sl")
-				if d == nil {
-					break
-				}
-				err.Traceback = append(err.Traceback, position.Position{
-					SourceName: "@" + d.ShortSource,
-					Line:       d.CurrentLine,
-					Column:     -1,
-				})
-
-				l++
-			}
+			th.trackError(err)
 		}
-
 		th.status = object.THREAD_ERROR
 		th.err = err
 	}
